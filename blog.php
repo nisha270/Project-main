71c9f1ad7c5e01cc4f1d52c9838c767c3a5f04be
nishaguptag <nishaguptag391@gmail.com>
2022-03-16 13:05:12 +0530

    final page

---
+++ blog.php
@@ -0,0 +1,82 @@
+<!-- navbaar -->
+<?php
+include "navbar.php";
+?>
+
+  <!-- main_page -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>beauty | contact</title>
+</head>
+<body>
+
+  <!-- 1st section page of blog -->
+  <section class="hero-wrap hero-wrap-2 about_page_head" style="background-image:url(https://preview.colorlib.com/theme/kayelux/images/xbg_2.jpg.pagespeed.ic.k2FHEGn_FG.webp); height: 739px;">
+    <div style="background-color: rgba(243, 213, 213, 0.397); height: 100vh;">
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-end">
+          <div class="col-md-9 mt-5">
+            <p class="breadcrumbs mt-123"><span class="me-2"><a href="index.html">Home <i class="fa fa-chevron-right"></i></a></span> <span>Blog<i class="fa fa-chevron-right"></i></span></p>
+            <h1 class="mb-5 bread">Blog</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="blog">
+    <div>
+      <h4 style="text-align: center;">Our Blog</h4>
+      <h2 class="text-center py-3">Recent From Blog</h2>
+      <div class="card-group">
+        <div class="card">
+          <a href="https://makeupandbeauty.com/"><img src="https://preview.colorlib.com/theme/kayelux/images/image_1.jpg" class="card-img-top" alt="image" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000"></a>
+          <div class="card-body">
+            <p>ADMIN MAR. 31, 2021 3 COMMENTS</p>
+            <h5 class="card-title text-muted">A facial scrub is basically a cream-based product that has some exfoliating
+              ingredients present in it that prevent your skin from damage</p>
+          </div>
+        </div>
+        <div class="card">
+          <a href="https://www.buywow.in/blogs/skin-science"><img src="https://static.toiimg.com/photo/msid-72368849/72368849.jpg?793304" class="card-img-top" alt="image" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000"></a>
+          <div class="card-body">
+            <p>ADMIN MAR. 31, 2021 3 COMMENTS</p>
+            <h5 class="card-title text-muted">The Beautyblender is a versatile makeup sponge that can be used for practically
+              anything in your makeup bag.</p>
+          </div>
+        </div>
+        <div class="card">
+          <a href="https://candycrow.com/"><img src="https://img.freepik.com/free-photo/young-beautiful-woman-with-flowers-near-face_186202-5624.jpg?size=626&ext=jpg" class="card-img-top" alt="..." data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000"></a>
+          <div class="card-body">
+            <p>ADMIN MAR. 31, 2021 3 COMMENTS</p>
+            <h5 class="card-title text-muted">Makeup artists are beauty practitioners who offer general makeup services or work in
+              prosthetic design.</h5>
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <div data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+            <a href="https://www.beautyandfashionfreaks.com/category/beauty/"><img src="https://preview.colorlib.com/theme/kayelux/images/image_4.jpg" class="card-img-top" alt="..."></a>
+          </div>
+          <div class="card-body">
+            <p>ADMIN MAR. 31, 2021 3 COMMENTS</p>
+            <h5 class="card-title text-muted">Nail art is a creative way to paint, decorate, enhance, and embellish nails. It is a
+              type of artwork that can be done on fingernails</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+</body>
+
+<!-- footer -->
+<?php
+include "footer.php";
+?>
+</html>
